abc5e1705a701c4e1208105ab7b621674f237701
Raul Soledispa <rsoledispa@technisys.com>
2022-07-19 17:21:29 -0500

    Redis integration as a cache of the application

--- apps/deduplication/src/app.js
+++ apps/deduplication/src/app.js
@@ -2,12 +2,23 @@ import Fastify from "fastify";
 import mongoClient from "@fastify/mongodb";
 import userRoutes from "./routes/index.js";
 import env  from "@fastify/env";
+import redisClient from "@fastify/redis";
+
 
 const schema = {
   type: "object",
   required: ["URL_DB"],
   properties: {
     URL_DB: {
+      type: "string"
+    },
+    CACHE_HOST: {
+      type: "string"
+    },
+    CACHE_PORT: {
+      type: "integer"
+    },
+    CACHE_PASSWORD: {
       type: "string"
     }
   }
@@ -33,7 +44,14 @@ export default async function Build() {
     useUnifiedTopology: true,
   });
 
-  app.register(userRoutes);
+  app.register(redisClient, {
+    host: app.config.CACHE_HOST,
+    password: app.config.CACHE_PASSWORD,
+    port: app.config.CACHE_PORT,
+    family: 4
+  })
+
+  await app.register(userRoutes);
 
   return app;
 }
--- apps/deduplication/src/routes/index.js
+++ apps/deduplication/src/routes/index.js
@@ -1,3 +1,6 @@
+import { createHash } from "crypto";
+
+
 async function routes(fastify) {
   fastify.get("/api/users", async (request, reply) => {
     const collection = fastify.mongo.client.db("test").collection("data");
@@ -5,12 +8,27 @@ async function routes(fastify) {
   });
 
   fastify.post("/api/users", async (req, reply) => {
-    const { user } = req.body
+    const { user, transactionId } = req.body
 
+    const hash = createHash("sha256").update(JSON.stringify(req.body)).digest("hex");
 
-    const collection = fastify.mongo.client.db("test").collection("data");
-    await collection.insertOne(user);
-    reply.send({ message: "OK" });
+    const cachedResult = await fastify.redis.get(transactionId);
+
+    if(!cachedResult) {
+      const collection = fastify.mongo.client.db("test").collection("data");
+      await collection.insertOne(user);
+      const response = { message: "User created" }
+      await fastify.redis.set(transactionId, JSON.stringify({hash, response}))
+      return response;
+    }
+
+    const cachedValue = JSON.parse(cachedResult);
+
+    if(hash === cachedValue.hash){
+      reply.send(cachedValue.response)
+    }else{
+      reply.code(409).send();
+    }
   });
 
   fastify.put("/api/users", async (req, reply) => {
